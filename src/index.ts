21bb80840657f5e549347c86ac370c3e02f91079
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2025-12-03 13:11:29 +0100

    Added button to create a new window

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import { Window } from "./elements/window.js";
+import { randomInt, screenHeight, screenWidth } from "./utils.js";
 
 customElements.define("c-window", Window);
+
+const newWindowBtn = document.getElementById("newWindowBtn") as HTMLButtonElement;
+
+const windows: Window[] = [];
+
+newWindowBtn.addEventListener("click", () => {
+  const width = randomInt(1, screenWidth());
+  const height = randomInt(1, screenHeight());
+
+  const x = randomInt(0, screenWidth() - width);
+  const y = randomInt(0, screenHeight() - height);
+
+  const window = new Window;
+
+  window.x = x;
+  window.y = y;
+
+  window.width = width;
+  window.height = height;
+
+  document.body.appendChild(window);
+});
