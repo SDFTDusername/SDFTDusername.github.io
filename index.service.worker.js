19c129383dc4e022d2508f4efe0f43074d1553e9
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2026-02-24 00:28:44 +0100

    Updated build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771067017|1150152405';
+const CACHE_VERSION = '1771889207|724527087';
 /** @type {string} */
 const CACHE_PREFIX = 'SDFTDusername-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
